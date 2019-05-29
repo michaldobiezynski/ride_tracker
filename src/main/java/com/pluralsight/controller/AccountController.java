b43005d326cd17dccb8741cac7a8170bd20b0500
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-05-29 21:20:09 +0100

    fetching user details from database

--- src/main/java/com/pluralsight/controller/AccountController.java
+++ src/main/java/com/pluralsight/controller/AccountController.java
@@ -5,6 +5,8 @@ import com.pluralsight.Services.AccountServiceImpl;
 import com.pluralsight.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -85,6 +87,9 @@ public class AccountController {
     public String listAccounts(Model model) {
         List<Account> accounts = accountService.getAccounts();
         model.addAttribute("accounts", accounts);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName(); //get logged in user name
+        model.addAttribute("username", name);
         return "listAccounts";
     }
 
